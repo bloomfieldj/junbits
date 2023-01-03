00e4e59461e5a3180ad81d5775cfbf2cf4db13fb
Jonathon Bloomfield <36716203+bloomfieldj@users.noreply.github.com>
2023-01-03 13:32:33 -0500

    update deps & clean index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
-  return (
-    <>
-      <h1>Junbits</h1>
-    </>
-  );
+export default function HomePage() {
+  return <h1>Junbits</h1>;
 }
