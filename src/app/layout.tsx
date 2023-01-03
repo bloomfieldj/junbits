d296ae79178850aa5abc07545bb66932c686ac24
Jonathon Bloomfield <36716203+bloomfieldj@users.noreply.github.com>
2023-01-03 17:52:51 -0500

    add supabase auth helpers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
+import "server-only";
+import SupabaseListener from "@/components/supabase-listener";
 import "@/app/globals.css";
+import createClient from "@/utils/supabase-server";
 
 import { Inter } from "@next/font/google";
 const inter = Inter({
@@ -6,16 +9,24 @@ const inter = Inter({
   variable: "--inter-font",
 });
 
-export default function RootLayout({
+export const revalidate = 0;
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const supabase = createClient();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
   return (
     <html lang="en" className="h-full scroll-smooth antialiased">
       <body
         className={`flex h-full flex-col bg-slate-1 text-slate-12 ${inter.variable} dark font-sans`}
       >
+        <SupabaseListener accessToken={session?.access_token} />
         {children}
       </body>
     </html>
