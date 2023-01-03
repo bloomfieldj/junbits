c36dc3fc7171d5d8595008b3831ffe764a0a97c4
Jonathon Bloomfield <36716203+bloomfieldj@users.noreply.github.com>
2023-01-03 00:30:23 -0500

    add supabase auth provider to app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,23 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
+import { useState } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{ initialSession: Session }>) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
   return (
     <ThemeProvider attribute="class" enableSystem>
-      <Component {...pageProps} />
+      <SessionContextProvider
+        supabaseClient={supabaseClient}
+        initialSession={pageProps.initialSession}
+      >
+        <Component {...pageProps} />
+      </SessionContextProvider>
     </ThemeProvider>
   );
 }
