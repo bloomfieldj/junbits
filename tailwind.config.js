c59a903f2106e7bd7442123fa78a4e779d60d902
Jonathon Bloomfield <36716203+bloomfieldj@users.noreply.github.com>
2023-01-03 11:01:57 -0500

    add src directory and update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [require("windy-radix-palette")],
 };
