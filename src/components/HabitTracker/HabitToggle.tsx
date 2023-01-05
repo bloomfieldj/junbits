275c257fbf7333954758011aafbc835b8070048b
Jonathon Bloomfield <36716203+bloomfieldj@users.noreply.github.com>
2023-01-05 04:01:41 -0500

    update habit toggle to mimic async mutation

---
+++ src/components/HabitTracker/HabitToggle.tsx
@@ -0,0 +1,30 @@
+import * as Toggle from "@radix-ui/react-toggle";
+import { useState } from "react";
+
+type Props = {
+  title: string;
+  children: React.ReactNode;
+};
+
+export default function HabitToggle({ title, children }: Props) {
+  const [complete, setComplete] = useState(false);
+  const [working, setWorking] = useState(false);
+
+  const handleToggle = async (toggled: boolean) => {
+    setWorking(true);
+    await new Promise((r) => setTimeout(r, 300));
+    setComplete(toggled);
+    setWorking(false);
+  };
+
+  return (
+    <Toggle.Root
+      aria-label={`Toggle ${title}`}
+      className="flex h-10 w-10 items-center justify-center rounded-md text-crimson-9 shadow-[0px_0px_10px] shadow-whiteA-9 hover:bg-sage-2 focus:shadow-[0px_0px_10px_5px] focus:shadow-whiteA-10 data-[state=on]:text-mint-9 data-[state=on]:shadow-none"
+      pressed={complete}
+      onPressedChange={(toggled) => handleToggle(toggled)}
+    >
+      {working ? <p>o</p> : children}
+    </Toggle.Root>
+  );
+}
--- src/components/HabitTracker/index.tsx
+++ src/components/HabitTracker/index.tsx
@@ -1,15 +1,19 @@
 "use client";
 
-import { Book, BookOpenCheckIcon } from "lucide-react";
-import HabitToggle from "./Toggle";
+import { Book, Edit3 } from "lucide-react";
+import HabitToggle from "./HabitToggle";
+import { useState } from "react";
 
 export default function HabitTracker() {
   return (
     <section className="my-6">
       <h3 className="text-xl">Daily Goals</h3>
-      <fieldset className="flex w-full gap-3">
+      <fieldset className="flex w-full gap-4">
         <HabitToggle title="Reading">
           <Book />
+        </HabitToggle>
+        <HabitToggle title="Journaling">
+          <Edit3 />
         </HabitToggle>
       </fieldset>
     </section>
